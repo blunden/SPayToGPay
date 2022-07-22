05c526e8e1b5e5c804e025fa744ac5d616f2d679
blunden <blunden2@gmail.com>
2022-07-22 14:58:42 +0200

    Revert "Support both Google Pay and Google Wallet
    Didn't work for reasons I don't feel like blindly troubleshooting
    
    This reverts commit 2006a95faec2dcf81decff69301f49143b5f770d.

--- app/src/main/java/com/samsung/android/samsungpay/gear/app/MainActivity.java
+++ app/src/main/java/com/samsung/android/samsungpay/gear/app/MainActivity.java
@@ -1,88 +1,32 @@
 package com.samsung.android.samsungpay.gear.app;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Intent;
-import android.content.pm.ComponentInfo;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class MainActivity extends Activity {
     private static final String TAG = "SPayToGPay";
-    private static final String COMPONENT_GOOGLE_PAY = "com.google.android.apps.walletnfcrel/com.google.commerce.tapandpay.android.wearable.cardlist.WearPayActivity";
-    private static final String COMPONENT_GOOGLE_WALLET = "com.google.android.apps.walletnfcrel/com.google.commerce.tapandpay.wear.cardlist.WalletThemedWearCardListActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Log.d(TAG, "Started MainActivity. Attempting to start Google Pay/Wallet.");
+        Log.d(TAG, "Started MainActivity. Attempting to start Google Pay.");
 
-        ComponentName googlePayComponent = ComponentName.unflattenFromString(COMPONENT_GOOGLE_PAY);
-        ComponentName googleWalletComponent = ComponentName.unflattenFromString(COMPONENT_GOOGLE_WALLET);
-
-        // Build intent to launch Google Pay/Wallet
+        // Build intent to launch Google Pay
         Intent activityIntent;
         activityIntent = new Intent(Intent.ACTION_MAIN);
-
-        if (isComponentEnabled(googleWalletComponent)) {
-            activityIntent.setComponent(googleWalletComponent);
-        } else if (isComponentEnabled(googlePayComponent)) {
-            activityIntent.setComponent(googlePayComponent);
-        }
+        activityIntent.setComponent(ComponentName.unflattenFromString("com.google.android.apps.walletnfcrel/com.google.commerce.tapandpay.android.wearable.cardlist.WearPayActivity"));
 
         try {
             startActivity(activityIntent);
 
         } catch (Exception e) {
-            Log.e(TAG, "Failed to start Google Pay/Wallet activity");
+            Log.e(TAG, "Failed to start Google Pay activity");
             e.printStackTrace();
         }
         finish();
-    }
-
-    @SuppressLint("SwitchIntDef")
-    private boolean isComponentEnabled(ComponentName componentName) {
-        PackageManager pm = getPackageManager();
-        int componentEnabledSetting = pm.getComponentEnabledSetting(componentName);
-
-        switch (componentEnabledSetting) {
-            case PackageManager.COMPONENT_ENABLED_STATE_DISABLED:
-                return false;
-            case PackageManager.COMPONENT_ENABLED_STATE_ENABLED:
-                return true;
-            case PackageManager.COMPONENT_ENABLED_STATE_DEFAULT:
-            default:
-                // Need to get the application info to get the component's default state
-                try {
-
-                    PackageInfo packageInfo = pm.getPackageInfo(componentName.getPackageName(),
-                            PackageManager.GET_ACTIVITIES | PackageManager.MATCH_DISABLED_COMPONENTS);
-
-                    List<ComponentInfo> components = new ArrayList<>();
-                    if (packageInfo.activities != null) {
-                        Collections.addAll(components, packageInfo.activities);
-                    }
-
-                    for (ComponentInfo componentInfo : components) {
-                        if (componentInfo.name.equals(componentName.getClassName())) {
-                            return componentInfo.isEnabled();
-                        }
-                    }
-
-                    // The component is not declared in the AndroidManifest or package disabled
-                    return false;
-                } catch (PackageManager.NameNotFoundException e) {
-                    // The package is not installed on the device
-                    return false;
-                }
-        }
     }
 }
